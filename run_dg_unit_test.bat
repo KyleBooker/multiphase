79fe771246b00709aaa38b57a06a2d2f3207e582
admin <bookerkyle@gmail.com>
2026-05-24 17:27:41 -0700

    chore: add batch wrappers for NGSolve scripts

---
+++ run_dg_unit_test.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=C:\Program Files\ngsolve-v6.2.2102\bin;%PATH%
+set PYTHONPATH=C:\Program Files\ngsolve-v6.2.2102\Lib\site-packages;%PYTHONPATH%
+"C:\Users\17785\AppData\Local\Programs\Python\Python37\python.exe" "%~dp0DG_INS_BDM_UNIT_TEST.py"
